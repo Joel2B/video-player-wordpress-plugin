764de8b77dc56ae30296110247c49f33e5ba286e
Joel2B <58922368+Joel2B@users.noreply.github.com>
2021-10-08 20:38:27 -0600

    Update links

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <title>Custom Video Player</title>
-        <script src="https://watchonline.nom.es/player/v1/current/player.min.js"></script>
+        <script src="https://appsdev.cyou/player/v1/current/player.min.js"></script>
         <link rel="stylesheet" href="<?php echo esc_url( CVP_URL . 'public/assets/css/styles.css?v=' . CVP_VERSION ); ?>" />
     </head>
     <body>
